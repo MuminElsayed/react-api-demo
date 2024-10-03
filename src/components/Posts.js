d58041dce8193cdf592114783d74a4ffae9abe71
MuminElsayed <master_mumin@hotmail.com>
2024-10-03 00:36:37 -0700

    init commit

---
+++ src/components/Posts.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { getPosts } from "../services/postService";
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts()
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return <></>;
+}
